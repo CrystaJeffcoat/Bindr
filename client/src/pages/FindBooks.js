0008f8b8ffd4d024cc29d7be9e9e94d5bfff2a1f
GPTdavenelson <60995712+GPTdavenelson@users.noreply.github.com>
2020-09-24 19:14:42 -0400

    SmallFix App Frame build

---
+++ client/src/pages/FindBooks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+ 
+const FindBooks = ({...props}) => {
+ 
+    return (
+        <div className={'root'}>
+            
+        </div>
+    );
+}
+ 
+ 
+export default FindBooks;
