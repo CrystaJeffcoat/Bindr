64d5b2ce64bd7c808b895e7ee609a994afc5c6da
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-26 14:36:30 -0400

    added book router to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
+const bookRoutes = require('./books');
 
-// Book routes
+// user routes
 router.use("/user", userRoutes);
+// book routes
+router.use('/books', bookRoutes);
 
 module.exports = router;
