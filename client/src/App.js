0c30508fdd4e58c49ab0c709b98539170f7a9734
blkmtnmouth <59021081+blkmtnmouth@users.noreply.github.com>
2020-09-26 09:57:47 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container } from '@material-ui/core';
-import AppNav from './components/AppNav';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "@material-ui/core";
+import AppNav from "./components/AppNav";
 
-import Books from './pages/Books';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Nav from './components/Nav';
-import AppFrame from './pages/AppFrame';
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import AppFrame from "./pages/AppFrame";
+import Login from "./pages/Login";
 
 function App() {
-    return (
-        <Router>
-            <Container maxWidth="sm">
-                <AppNav />
-                <Switch>
-                    <Route exact path={['/', '/books']}>
-                        <AppFrame />
-                    </Route>
-                    <Route exact path="/books/:id">
-                        <Detail />
-                    </Route>
-                    <Route>
-                        <NoMatch />
-                    </Route>
-                </Switch>
-            </Container>
-        </Router>
-    );
+  return (
+    <Router>
+      <Container maxWidth="sm">
+        <AppNav />
+        <Switch>
+          <Route exact path="/books">
+            <AppFrame />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route path="/NoMatch">
+            <NoMatch />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
