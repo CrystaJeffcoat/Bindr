7b8e2bfbad7dcb2a40fa015b6bbd3b3638a4fb3e
blkmtnmouth <59021081+blkmtnmouth@users.noreply.github.com>
2020-09-28 15:08:02 -0400

    Add files via upload

---
+++ client/src/components/NewUserBtn/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+function NewUserBtn(props) {
+  let history = useHistory();
+  let username = props.userName;
+  let password = props.password;
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    fetch("/add", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    }).then(function (response) {
+      return response.json();
+    });
+
+    //fetch("/api/user/favorites").then(function (response) {
+    //history.push("/home");
+    // });
+
+    history.push("/home");
+  }
+  const classes = makeStyles((theme) => ({
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  return (
+    <Button
+      type="submit"
+      variant="contained"
+      color="primary"
+      fullWidth
+      className={classes.submit}
+      onClick={handleFormSubmit}
+    >
+      Create New User
+    </Button>
+  );
+}
+
+export default NewUserBtn;
