7fcc991c0995e7fc6269ab6754d018e155267d90
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-29 13:32:52 -0400

    path changes for fetch routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
@@ -10,27 +10,28 @@ import SavedBooks from './pages/SavedBooks';
 import Login from './pages/Login';
 import './index.css';
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
+class App extends Component {
+  render() {
+  return(
+    <ThemeProvider theme = { theme } >
       <Router>
         {/*  
                 Mui StylesProvider allows our styles to override Material-UI 
                 styles - https://material-ui.com/styles/advanced/#injectfirst 
                 */}
         <StylesProvider injectFirst>
-        <Container fixed maxWidth="xs" className="app-container">
-          <Switch>
-            <Route exact path={'/'}>
-              <Login />
-            </Route>
-            <Route exact path={'/home'}>
-              <AppNav />
-              <AppFrame />
-            </Route>
-            <Route exact path="/saved">
-              <AppNav />
-              <SavedBooks />
+          <Container fixed maxWidth="xs" className="app-container">
+            <Switch>
+              <Route exact path={'/'}>
+                <Login />
+              </Route>
+              <Route exact path={'/home'}>
+                <AppNav />
+                <AppFrame />
+              </Route>
+              <Route exact path="/saved" render={() => <LoginPage onUsernameChange={this.onUsernameChange}>
+                <AppNav />
+                <SavedBooks />
             </Route>
           </Switch>
         </Container>
@@ -38,6 +39,7 @@ function App() {
       </Router>
     </ThemeProvider>
   );
+}
 }
 
 export default App;
