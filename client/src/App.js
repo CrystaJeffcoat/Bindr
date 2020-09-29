8cf93deef22c44968449e2d20d61a59fa405f0af
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-29 16:29:33 -0400

    fixed App file

--- client/src/App.js
+++ client/src/App.js
@@ -12,34 +12,32 @@ import './index.css';
 
 class App extends Component {
   render() {
-  return(
-    <ThemeProvider theme = { theme } >
-      <Router>
-        {/*  
-                Mui StylesProvider allows our styles to override Material-UI 
-                styles - https://material-ui.com/styles/advanced/#injectfirst 
-                */}
-        <StylesProvider injectFirst>
-          <Container fixed maxWidth="xs" className="app-container">
-            <Switch>
-              <Route exact path={'/'}>
-                <Login />
-              </Route>
-              <Route exact path={'/home'}>
-                <AppNav />
-                <AppFrame />
-              </Route>
-              <Route exact path="/saved" render={() => <LoginPage onUsernameChange={this.onUsernameChange}>
-                <AppNav />
-                <SavedBooks />
-            </Route>
-          </Switch>
-        </Container>
-      </StylesProvider>
-      </Router>
-    </ThemeProvider>
-  );
-}
+    return (
+      <ThemeProvider theme={theme} >
+        <Router>
+          {/*Mui StylesProvider allows our styles to override Material-UI 
+          styles - https://material-ui.com/styles/advanced/#injectfirst*/}
+          <StylesProvider injectFirst>
+            <Container fixed maxWidth="xs" className="app-container">
+              <Switch>
+                <Route exact path='/'>
+                  <Login />
+                </Route>
+                <Route exact path='/home'>
+                  <AppNav />
+                  <AppFrame />
+                </Route>
+                <Route exact path="/saved">
+                  <AppNav />
+                  <SavedBooks />
+                </Route>
+              </Switch>
+            </Container>
+          </StylesProvider>
+        </Router>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
